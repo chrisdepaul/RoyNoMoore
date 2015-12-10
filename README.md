c69d7d14832e766e4e005ed5088b290854732413
Pratik Todi <ptodi.y12@lnmiit.ac.in>
2015-12-10 11:34:02 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+### A chrome extension that replaces following words in a web-page after it has been loaded
+- 'Refugee' to 'Human'
+- 'Refugees' to 'Humans'
---
+++ content.js
@@ -0,0 +1,19 @@
+var elements = document.getElementsByTagName('*');
+
+for (var i = 0; i < elements.length; i++) {
+    var element = elements[i];
+
+    for (var j = 0; j < element.childNodes.length; j++) {
+        var node = element.childNodes[j];
+
+        if (node.nodeType === 3) {
+            var text = node.nodeValue;
+            var replacedText = text.replace(/Refugee/g, "Human");
+            replacedText = replacedText.replace(/refugee/g, "human");
+
+            if (replacedText !== text) {
+                element.replaceChild(document.createTextNode(replacedText), node);
+            }
+        }
+    }
+}
